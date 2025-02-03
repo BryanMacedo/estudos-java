1647355287d3e43ebff449f4c35739c1d206de71
Bryan Macedo <contato.bryansm@hotmail.com>
2025-02-03 11:12:33 -0300

    Estudos sobre exceptions PT1.

---
+++ src/javaProgramacaoOrientadaObjetos/Oexceptions/Runtime/test/RuntimeExceptionTest01.java
@@ -0,0 +1,9 @@
+package javaProgramacaoOrientadaObjetos.Oexceptions.Runtime.test;
+
+public class RuntimeExceptionTest01 {
+    public static void main(String[] args) {
+        // checked e unchecked
+        int[] nusms = {1 ,2};
+        System.out.println(nusms[2]);
+    }
+}

f5f6e1df1e8588922040058e13b291d11f6c291e
Bryan Macedo <contato.bryansm@hotmail.com>
2025-02-07 13:18:45 -0300

    Estudos sobre horas e datas PT2.

---
+++ src/javaProgramacaoOrientadaObjetos/Rdate/test/ChronoUnitTest01.java
@@ -0,0 +1,39 @@
+package javaProgramacaoOrientadaObjetos.Rdate.test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+public class ChronoUnitTest01 {
+    public static void main(String[] args) {
+        LocalDateTime aniversaio = LocalDateTime.of(1988, Month.AUGUST, 6, 12,0,0);
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(ChronoUnit.DAYS.between(aniversaio, now));
+        System.out.println(ChronoUnit.WEEKS.between(aniversaio, now));
+        System.out.println(ChronoUnit.MONTHS.between(aniversaio, now));
+        System.out.println(ChronoUnit.YEARS.between(aniversaio, now));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/javaProgramacaoOrientadaObjetos/Rdate/test/DuratioinTest01.java
@@ -0,0 +1,52 @@
+package javaProgramacaoOrientadaObjetos.Rdate.test;
+
+
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+
+public class DuratioinTest01 {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime nowAfter2Yaers = LocalDateTime.now().plusYears(2).plusMinutes(8);
+        LocalTime timeNow = LocalTime.now();
+        LocalTime timeMinus7Hours = LocalTime.now().minusHours(7);
+
+        Duration d1 = Duration.between(now, nowAfter2Yaers);
+        Duration d2 = Duration.between(timeNow, timeMinus7Hours);
+        Duration d3 = Duration.between(Instant.now(), Instant.now().plusSeconds(1000));
+        Duration d4 = Duration.ofDays(20);
+        Duration d5 = Duration.ofMinutes(3);
+        Duration d6 = Duration.of(3, ChronoUnit.HOURS);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3);
+        System.out.println(d4);
+        System.out.println(d5);
+        System.out.println(d6);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

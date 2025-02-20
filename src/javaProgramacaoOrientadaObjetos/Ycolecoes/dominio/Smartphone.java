0f8a534a79349a216c77a70eb9bee2df86762a81
Bryan Macedo <contato.bryansm@hotmail.com>
2025-02-20 12:36:48 -0300

    Estudos sobre collections utilizando o hashCode PT2.

--- src/javaProgramacaoOrientadaObjetos/Ycolecoes/dominio/Smartphone.java
+++ src/javaProgramacaoOrientadaObjetos/Ycolecoes/dominio/Smartphone.java
@@ -23,6 +23,15 @@ public class Smartphone {
         return serialNumber != null && serialNumber.equals(smartphone.serialNumber);
     }
 
+    //se x.equals(y) == true, y.hashCode() == x.hashCode()
+    //y.hashCode() == x.hashCode() n necessariamente o equals de y.equals(x) tem q ser true
+    //x.equals(y) == false os hashCode tem q ser diferentes
+    //y.hashCode() != x.hashCode() x.equals(y) deverá ser falso
+    @Override
+    public int hashCode() {
+        return serialNumber == null ? 0 : this.serialNumber.hashCode();
+    }
+
     public String getSerialNumber() {
         return serialNumber;
     }

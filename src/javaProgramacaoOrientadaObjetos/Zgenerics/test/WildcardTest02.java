14230b6ac3831081d320cfcf3953af5e54acf253
Bryan Macedo <contato.bryansm@hotmail.com>
2025-04-05 09:54:58 -0300

    Estudos sobre Generics-Wildcard PT2.

--- src/javaProgramacaoOrientadaObjetos/Zgenerics/test/WildcardTest02.java
+++ src/javaProgramacaoOrientadaObjetos/Zgenerics/test/WildcardTest02.java
@@ -1,23 +1,33 @@
 package javaProgramacaoOrientadaObjetos.Zgenerics.test;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WildcardTest02 {
     public static void main(String[] args) {
         List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
         List<Gato> gatos = List.of(new Gato(), new Gato());
+        printConsulta(cachorros);
+        printConsulta(gatos);
+        List<Animal> animals = new ArrayList<>();
+        printConsultaAnimal(animals);
 
     }
 
-    //type erasure
-    private static void printConsulta(List<Animal> animals){
+    //type erasure ?                         extends é para classes e interfaces
+    private static void printConsulta(List<? extends Animal> animals) {
         for (Animal animal : animals) {
             animal.consulta();
         }
+    }
+
+    private static void printConsultaAnimal(List<? super Animal> animals) {
+        Animal animal = new Cachorro();
+        Animal animal2 = new Gato();
 
         animals.add(new Cachorro());
-
+        animals.add(new Gato());
     }
 }
 

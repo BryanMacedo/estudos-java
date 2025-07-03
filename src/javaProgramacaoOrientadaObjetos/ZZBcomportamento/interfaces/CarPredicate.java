960d903a6ac19ff3c08786b520952802bc58caff
Bryan Macedo <contato.bryansm@hotmail.com>
2025-07-03 14:41:12 -0300

    Parametrizando comportamentos pt2.

---
+++ src/javaProgramacaoOrientadaObjetos/ZZBcomportamento/interfaces/CarPredicate.java
@@ -0,0 +1,7 @@
+package javaProgramacaoOrientadaObjetos.ZZBcomportamento.interfaces;
+
+import javaProgramacaoOrientadaObjetos.ZZBcomportamento.dominio.Car;
+
+public interface CarPredicate {
+    boolean test(Car car);
+}
